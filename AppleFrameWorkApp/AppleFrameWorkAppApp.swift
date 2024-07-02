c3c42c3c1563e63c36048f13958fba6271bcad72
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-03 01:40:56 +0500

    Initial Commit

---
+++ AppleFrameWorkApp/AppleFrameWorkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameWorkAppApp.swift
+//  AppleFrameWorkApp
+//
+//  Created by Muhammad Zeshan on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameWorkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
