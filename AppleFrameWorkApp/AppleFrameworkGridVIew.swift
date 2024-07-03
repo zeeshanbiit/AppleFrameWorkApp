78762c7ddb69c21f31862b288d7fce5b4cb404a8
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-04 04:21:56 +0500

    Change Main ContentView Name and File to CustomView and Name

---
+++ AppleFrameWorkApp/AppleFrameworkGridVIew.swift
@@ -0,0 +1,18 @@
+//
+//  AppleFrameworkGridVIew.swift
+//  AppleFrameWorkApp
+//
+//  Created by Muhammad Zeshan on 04/07/2024.
+//
+
+import SwiftUI
+
+struct AppleFrameworkGridVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppleFrameworkGridVIew()
+}
